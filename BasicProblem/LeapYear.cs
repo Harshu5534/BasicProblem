4b6a50bf447e18e1ac32ea61550d51926e4305cb
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-11 13:44:36 +0530

    [Harshal]Add: Check Year is Leap Or Not

---
+++ BasicProblem/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class LeapYear
+    {
+        public void CheckLeapYear()
+        {
+            Console.WriteLine("Enter The Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine(year + " Is Leap Year ");
+            }
+            else
+            {
+                Console.WriteLine(year + " Is Not Leap Year ");
+            }
+
+        }
+    }
+}
--- BasicProblem/Program.cs
+++ BasicProblem/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Basic Program");
+﻿using System;
+namespace BasicProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Program");
+            bool end = true;
+            Console.WriteLine("1.Leap Year\n 2.End the Program");
+            while (end == true)
+            {
+                Console.WriteLine("Take an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.CheckLeapYear();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Proper Option To Execute");
+                        break;
+                }
+            }
+        }
+    }
+}
